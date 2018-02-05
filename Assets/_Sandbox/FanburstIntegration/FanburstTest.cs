ce08b015268a26591b7db71338a713c6046e1c4a
Yuta Nakagawa <nakagawa@1-10.com>
2018-02-05 14:05:44 +0900

    failed to use spotify and soundcloud

---
+++ Assets/_Sandbox/FanburstIntegration/FanburstTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace LABVR.FanburstIntegration
+{
+
+    public class FanburstTest : MonoBehaviour
+    {
+        [SerializeField]
+        private string m_ClientId = "";
+
+        private readonly string BASE_URL = "https://api.fanburst.com";
+
+        private string m_AccessToken = "b25531438172b74d74841e6676f45cb54a711a43b544dcb1098aaf53589c389a";
+
+
+        private IEnumerator ExcuteAPI ()
+        {
+            UnityWebRequest request = UnityWebRequest.Get("https://example.com");
+            request.SetRequestHeader();
+        }
+
+    }
+
+}
