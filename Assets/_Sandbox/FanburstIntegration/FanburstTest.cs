31f515fe63613c2196dc3424ac43db8fcea606af
Yuta Nakagawa <nakagawa@1-10.com>
2018-11-15 21:29:46 +0900

    update to unity 2018.2.5f

--- Assets/_Sandbox/FanburstIntegration/FanburstTest.cs
+++ Assets/_Sandbox/FanburstIntegration/FanburstTest.cs
@@ -4,7 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Net;
 using System.Threading;
-using System.Web;
+//using System.Web;
 using UnityEngine;
 using UnityEngine.Networking;
 
